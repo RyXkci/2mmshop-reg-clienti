7266995dd75418e060033a42b3981658d0cfd307
Ricardo <ricardobozzi23@gmail.com>
2024-08-13 17:47:43 +0200

    Wrote basic filter function, non destructive, can constantly filter initial data

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,70 @@
-import { useState, useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
+  const [initialData, setInitialData] = useState([]);
   const [users, setUsers] = useState([])
 
+  const getData = async () => {
+    const response = await fetch("http://localhost:3000/users");
+    const json = await response.json();
+    setInitialData(json);
+    setUsers(json)  //on each initial render all of them
+    console.log(json);
+  };
+
   useEffect(() => {
-    async function getData() {
-      const response = await fetch('http://localhost:3000/users');
-      const json = await response.json();
-      setUsers(json)
-      console.log(json)
+    getData();
+  }, []);
 
-    }
-    getData()
-  },[])
 
+
+  const filterBySize = (param) => {
+    //filters rendered data by parameter without losing initial data. Can constantly filter by chosen param;
+    const filteredUsers = initialData.filter((user) => user.sizes.tShirt === param)
+    setUsers(filteredUsers);
+  }
+
+
+  console.log('users:', users)
 
   return (
-    <ul>
-      {users.map((user) => {
-        return <li>{user.name}</li>
-      })}
-    </ul>
-  )
+    <>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li>
+              {user.name} - t-shirt size: {user.sizes.tShirt}
+            </li>
+          );
+        })}
+      </ul>
+
+      <button
+        onClick={() => {
+          filterBySize("s");
+        }}
+      >
+        filter by s
+      </button>
+      <button
+        onClick={() => {
+          filterBySize("m");
+        }}
+      >
+        filter by m
+      </button>
+      <button
+        onClick={() => {
+          filterBySize("l");
+        }}
+      >
+        filter by l
+      </button>
+    </>
+  );
 }
 
-export default App
+export default App;
