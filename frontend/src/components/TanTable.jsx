59390fab33c011f2223116e4922cea094f9cdd35
Ricardo <ricardobozzi23@gmail.com>
2024-09-27 14:01:23 +0200

    Fixed problem where dropdowns weren't reverting to empty on reset

--- frontend/src/components/TanTable.jsx
+++ frontend/src/components/TanTable.jsx
@@ -1,123 +1,135 @@
-import { useState, useMemo, useEffect} from "react";
-import { useReactTable, getCoreRowModel, getFilteredRowModel, flexRender } from "@tanstack/react-table";
+import { useState, useMemo, useEffect } from "react";
+import {
+  useReactTable,
+  getCoreRowModel,
+  getFilteredRowModel,
+  flexRender,
+} from "@tanstack/react-table";
 import { COLUMNS } from "./columns";
- 
+
 import { getData } from "../utils/fetches";
- 
- export default function TanTable() {
-    const columns = useMemo(() => COLUMNS)
-    const [data, setData] = useState([]);
-    const [columnFilters, setColumnFilters] = useState([])
 
-    useEffect(() => {
-      const admin = JSON.parse(localStorage.getItem("admin"));
-      console.log(admin) 
-      const fetchData = async() => {
-        const response = await getData(admin)
-        if (!response.error) {
-          setData(response)
-        } else {
-          console.log(response)
-        }
+export default function TanTable() {
+  const columns = useMemo(() => COLUMNS);
+  const [data, setData] = useState([]);
+
+  const [columnFilters, setColumnFilters] = useState([]); //State for column filters. Initially empty
+  const [selectValues, setSelectValues] = useState({}); // State to track selected values for each header
+
+  useEffect(() => {
+    const admin = JSON.parse(localStorage.getItem("admin")); //checking auth
+    const fetchData = async () => {
+      const response = await getData(admin);
+      if (!response.error) {
+        setData(response);
+      } else {
+        return;
+        // console.log(response)
       }
+    };
 
-      if (admin) {
-        fetchData()
-      }
-    
-    }, [])
-
-
-    const tableInstance = useReactTable({ 
-      columns, data, 
-      state: {
-        columnFilters
-      },
-      getCoreRowModel: getCoreRowModel(),
-     
-      getFilteredRowModel: getFilteredRowModel()
-    })
-
-    const {
-        getHeaderGroups,
-        getRowModel,
-    } = tableInstance;
-
-    const reset = () => {
-      setColumnFilters([])
+    if (admin) {
+      fetchData();
     }
+  }, []);
 
-// const onFilterChange = (id, value) => setColumnFilters(
-//   prev => prev.filter(f => f.id !==id).concat({id, value})
-// )
+  const tableInstance = useReactTable({
+    columns,
+    data,
+    state: {
+      columnFilters,
+    },
+    getCoreRowModel: getCoreRowModel(),
 
-const onFilterChange = (id, event) => {
-  const value = event.target.value; // Get the value from the event
-  setColumnFilters(prev => 
-    prev.filter(f => f.id !== id) // Remove existing filter with the same id
-       .concat({ id, value })      // Add the new filter
+    getFilteredRowModel: getFilteredRowModel(),
+  });
+
+  const { getHeaderGroups, getRowModel } = tableInstance;
+
+  const onFilterChange = (id, event) => {
+    const value = event.target.value; // Get the value from the event
+    setColumnFilters(
+      (prev) =>
+        prev
+          .filter((f) => f.id !== id) // Remove existing filter with the same id
+          .concat({ id, value }) // Add the new filter
+    );
+    setSelectValues((prev) => ({ ...prev, [id]: value }));
+  };
+
+  const reset = () => {
+    setColumnFilters([]); //reset all filters to empty
+    setSelectValues({}); //reset all select dropdowns to empty
+  };
+
+  return (
+    <main className="main">
+      <button onClick={reset}>Reset</button>
+      <table>
+        <thead>
+          {getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                //dynamically add classes for background based on metadata
+                <th
+                  className={header.column.columnDef.meta?.className ?? ""}
+                  key={header.id}
+                >
+                  {header.column.columnDef.meta.hasFilter === true ? ( //checks if columns has filter in metadata. Not all columns should be filterable
+                    <>
+                      <label htmlFor={header.id}>
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      </label>
+                      <select
+                        name="header-options"
+                        value={selectValues[header.id] || ""} //changes value to header.id to easily revert to empty
+                        id={header.id}
+                        onChange={(event) => {
+                          onFilterChange(header.id, event);
+                        }}
+                      >
+                        <option value=""></option> //inital empty option //
+                        mapping over filterable columns to make options for
+                        dropdown based on metadata
+                        {header.column.columnDef.meta.selectOptions?.map(
+                          (option) => {
+                            return (
+                              <option key={option} value={option}>
+                                {option}
+                              </option>
+                            );
+                          }
+                        )}
+                      </select>
+                    </> //non filterable columns just render headers with no select dropdown
+                  ) : (
+                    flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {getRowModel().rows.map((row) => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </main>
   );
-};
-
-    return (
-      <main className="main">
-        <button onClick={reset}>Reset</button>
-        <table>
-         <thead>
-               {getHeaderGroups().map(headerGroup =>(
-                <tr key={headerGroup.id}>
-                    {headerGroup.headers.map(header => (
-                        <th className={header.column.columnDef.meta?.className ?? ""} key={header.id}>
-                        
-                          {header.column.columnDef.meta.hasFilter === true ?
-                          <><label htmlFor={header.id}>{flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}</label> 
-                          <select 
-                          name="header-options" 
-                          id={header.id}
-                          onChange={(event) => {onFilterChange(header.id, event)}}
-                          >
-                            <option value=""></option>
-                            
-                          {header.column.columnDef.meta.selectOptions?.map((option) => {
-                            return <option
-                            key={option}
-                            value={option}
-                            >{option}
-                            </option>
-                          })} 
-                          </select>
-                         
-                          </>: 
-                          flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )
-                      }
-                        </th>
-                    ))}
-                </tr>
-               ))}
-            </thead>
-            <tbody>
-              {getRowModel().rows.map(row => {
-                 return <tr key={row.id}>
-                 {row.getVisibleCells().map((cell) => (
-                   <td key={cell.id}>
-                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                   </td>
-                 ))}
-               </tr>
-              })}
-            </tbody>
-        </table>
-
-
-        
-        </main>
-
-    )
-
- }
+}
