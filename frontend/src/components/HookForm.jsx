9c8d8d3ccb03edefe7ed602451f2cafecc8e6756
Ricardo <ricardobozzi23@gmail.com>
2024-10-04 16:18:45 +0200

    Changed form title

--- frontend/src/components/HookForm.jsx
+++ frontend/src/components/HookForm.jsx
@@ -68,8 +68,8 @@ export default function HookForm() {
     <main className="main">
       <div className="form-container">
         <section className="form-header">
-          <h1 className="form-title">Titolo</h1>
-          <p className="form-subtitle">Testo per inserimento taglie</p>
+          <h1 className="form-title">Mystilebox</h1>
+          <p className="form-subtitle">Inserisci le tue taglie per ricevere promozioni su misura!</p>
         </section>
 
         <form className="post-form" onSubmit={handleSubmit(submitData)}>
