b55c4623395f4394d8f3207e91c951ff730842f1
Ricardo <ricardobozzi23@gmail.com>
2024-12-16 19:35:43 +0100

    made changes to backend match router

--- frontend/src/components/ClothesForm.jsx
+++ frontend/src/components/ClothesForm.jsx
@@ -5,13 +5,13 @@ export default function ClothesForm({
   formType,
   sizes,
   values,
+  imgPreviews,
   categories,
   registerOptions,
   handleFileChange,
   handleSave,
 }) {
-
-console.log(formType)
+  console.log(formType);
 
   const {
     register,
@@ -47,13 +47,21 @@ console.log(formType)
           type="file"
           name="images"
           multiple
-          {...register("images", registerOptions.images)}
-          // onChange={handleFileInputChange}
+          {...register("images")}
+          onChange={handleFileInputChange}
         />
         {errors?.images && (
           <div className="form-danger"> {errors.images.message}</div>
         )}
       </div>
+
+      {imgPreviews.length > 1 && (
+        <div className="images-container">
+          {imgPreviews.map((img) => {
+            return <img src={img} alt="" key={uuid()} />;
+          })}
+        </div>
+      )}
 
       <div className="clothes-input clothes-type-input">
         <label htmlFor="clothesType">Tipo:</label>
@@ -80,11 +88,12 @@ console.log(formType)
 
       <div className="clothes-input clothes-category-input">
         <label htmlFor="clothesCategory">Nome:</label>
-      <input type="text"
-      id="clothesCategory"
-      name="category"
-      {...register("category", registerOptions.category)}
-      />
+        <input
+          type="text"
+          id="clothesCategory"
+          name="category"
+          {...register("category", registerOptions.category)}
+        />
       </div>
 
       <div className="clothes-input clothes-description-container">
@@ -113,27 +122,30 @@ console.log(formType)
                 })}
               </select> */}
 
-      <div className="clothes-input clothes-size-input">
-        <fieldset>
-          <legend>Taglie</legend>
-          <div className="size-checkboxes">
-            {sizes.map((size) => {
-              return (
-                <div key={uuid()} className="size-checkbox">
-                  <label htmlFor="{size}">{size}</label>
-                  <input
-                    id={size}
-                    type="checkbox"
-                    key={size}
-                    value={size}
-                    {...register("sizes", registerOptions.sizes)}
-                  />
-                </div>
-              );
-            })}
-          </div>
-        </fieldset>
-      </div>
+      {formType !== "accessories" && (
+        <div className="clothes-input clothes-size-input">
+          <fieldset>
+            <legend>Taglie</legend>
+            <div className="size-checkboxes">
+              {sizes.map((size) => {
+                return (
+                  <div key={uuid()} className="size-checkbox">
+                    <label htmlFor="{size}">{size}</label>
+                    <input
+                      id={size}
+                      type="checkbox"
+                      key={size}
+                      value={size}
+                      {...register("sizes", registerOptions.sizes)}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </fieldset>
+        </div>
+      )}
+
       <div className="clothes-input clothes-sex-input">
         <label htmlFor="clothesSex">Sesso:</label>
         <select
@@ -162,7 +174,9 @@ console.log(formType)
         />
       </div>
       {/* <button onClick={() => handleSave(clothesData)}>Salva</button> */}
-      <button type="submit">Salva</button>
+      <button className="clothes-form__btn" type="submit">
+        Salva
+      </button>
     </form>
   );
 }
--- frontend/src/components/ClothingItem.jsx
+++ frontend/src/components/ClothingItem.jsx
@@ -1,44 +1,53 @@
 import { v4 as uuid } from "uuid";
 
-export default function ClothingItem({item}) {
-
-    return (
-        <div key={uuid()} className="clothes-container">
-                    <div
-                      key={uuid()}
-                      className="clothes-images images-container"
-                    >
-                      {console.log("ITEM IS", item)}
-                      {item.imgPreviews.map((img) => (
-                        <img
-                          src={img}
-                          alt=""
-                          style={{ width: "100px", display: "block" }}
-                          key={uuid()}
-                        />
-                      ))}
-                    </div>
-                    <div key={uuid()} className="clothes-details">
-                      <p key={uuid()}  className="clothes-details-text type">
-                        Tipo: {item.type}
-                      </p>
-                      {/* <p key={uuid()}  className="clothes-details-text size">
+export default function ClothingItem({ item }) {
+  return (
+    <div key={uuid()} className="clothes-container">
+      <div key={uuid()} className="clothes-images images-container">
+        {console.log("ITEM IS", item)}
+        {item.imgPreviews.map((img) => (
+          <img
+            src={img}
+            alt=""
+            style={{ width: "100px", display: "block" }}
+            key={uuid()}
+          />
+        ))}
+      </div>
+      <div key={uuid()} className="clothes-details">
+        <p key={uuid()} className="clothes-details-text type">
+          <span className="clothes-details-text__title">Tipo:</span> {item.type}
+        </p>
+        {/* <p key={uuid()}  className="clothes-details-text size">
                         Taglia: {item.size}
                       </p> */}
-                      <div className="clothes-sizes">
-                        <p>Taglie:</p>
-                        {item.sizes.map((size) => <p className="clothing-size">{size}</p>)}
-                      </div>
-                      <p key={uuid()}  className="clothes-details-text sex">
-                        Sesso: {item.sex}
-                      </p>
-                      <p key={uuid()}  className="clothes-details-text price">
-                        Prezzo: {item.price}
-                      </p>
-                      <p key={uuid()}  className="clothes-details-text discounted-price">
-                        Sconto: {item.discountedPrice}
-                      </p>
-                    </div>
-                    </div>
-    )
+        <p className="clothes-details-text name">
+          <span className="clothes-details-text__title">Nome: </span>
+          {item.category}
+        </p>
+        <div className="clothes-sizes">
+          <p className="clothes-details-text__title">Taglie:</p>
+          {item.sizes.map((size) => (
+            <p className="clothing-size">{size}</p>
+          ))}
+        </div>
+        <p key={uuid()} className="clothes-details-text sex">
+          <span className="clothes-details-text__title">Sesso: </span>{" "}
+          {item.sex}
+        </p>
+        <p key={uuid()} className="clothes-details-text description">
+
+        <span className="clothes-details-text__title">Descrizione: </span>{" "}
+        {item.description}
+        </p>
+        <p key={uuid()} className="clothes-details-text price">
+          <span className="clothes-details-text__title">Prezzo: </span>
+          {item.price}
+        </p>
+        <p key={uuid()} className="clothes-details-text discounted-price">
+          <span className="clothes-details-text__title">Sconto:</span>{item.discountedPrice}
+        </p>
+      </div>
+    </div>
+  );
 }
