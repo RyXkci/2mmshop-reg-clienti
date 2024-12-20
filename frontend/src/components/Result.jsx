e21101863b8662fe283839bb0ac634b7653cba74
Ricardo <ricardobozzi23@gmail.com>
2024-12-20 16:58:51 +0100

    Added icons to success page

--- frontend/src/components/Result.jsx
+++ frontend/src/components/Result.jsx
@@ -1,8 +1,19 @@
 import "../stylesheets/result.css";
 
+//icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
+
+const instaIcon = <FontAwesomeIcon icon={faInstagram} />;
+const facebookIcon = <FontAwesomeIcon icon={faFacebook} />;
+
+console.log(instaIcon);
+console.log(facebookIcon);
+
 import Logo from "./Logo";
 
-export default function Result({ content, clickFunc }) {
+export default function Result({ content, clickFunc, isSuccess }) {
+  console.log(isSuccess);
   return (
     //   <div className="result-page">
     //   <div className={`result-page-inner ${content.class}`}>
@@ -14,15 +25,13 @@ export default function Result({ content, clickFunc }) {
 
     <main className="result-page">
       <div className="result-container">
-      <section className={`result-content ${content.class}`}>
+        <section className={`result-content ${content.class}`}>
           <h1 className="result-page__title">{content.title}</h1>
           <p className="result-page__subtitle">{content.body}</p>
         </section>
 
-        <Logo
-        size="bg"
-        />
-       
+        <Logo size="bg" />
+
         {/* <section className="logo-container-circle bg">
           <h1 className="logo__title">
             <span className="logo__title-large">2</span>
@@ -31,8 +40,34 @@ export default function Result({ content, clickFunc }) {
           </h1>
           <p className="logo__subtitle">MyStyleBox</p>
         </section> */}
-          <button className="btn-fill has-top-border-shadow" onClick ={clickFunc}>{content.button}</button>
+        {/* <button className="btn-fill has-top-border-shadow" onClick ={clickFunc}>{content.button}</button> */}
 
+        {isSuccess && (
+          <div className="result-page__icon-container">
+            <h2 className="result-page__icon-container__title result-page__title">
+              Seguici su:
+            </h2>
+            <div className="social-icons">
+              <a
+                href="https://www.instagram.com/2mmshop.it?igsh=MWdocndzNmducXhoaA=="
+                target="blank"
+                className="social-icon insta-icon"
+              >
+                {instaIcon}
+              </a>
+              <a
+                href="https://www.facebook.com/share/18UGQqBDn5/"
+                target="blank"
+                className="social-icon fb-icon"
+              >
+                {facebookIcon}
+              </a>
+            </div>
+          </div>
+        )}
+        {/* <span>{instaIcon}</span>
+          <span>{facebookIcon}</span>
+          <a href="">Hello</a> */}
       </div>
     </main>
   );
