f045518e89d6025f04be1add87ba02a12abb1ed8
Ricardo <ricardobozzi23@gmail.com>
2025-01-22 16:28:57 +0100

    Changes header intro colors

--- frontend/src/components/ClothesShowHeader.jsx
+++ frontend/src/components/ClothesShowHeader.jsx
@@ -9,10 +9,11 @@ export default function ClothesShowHeader() {
     <div className="clothes-show-header">
       <div className="clothes-show-header__logo">
         <img src={logoBlack} alt="" />
+        <p>dal 1973</p>
         <h1 className="clothes-show-header-logo__text">Manfredi Giordano</h1>
       </div>
-      <div className="clothes-show-header__title"><p>Il piacere di indossare ciò che vi rende unici</p></div>
-      <div className="clothes-show-header__intro"><p>Ciao utente, ecco la selezone di capi in promozione esclusivi della tua taglia</p></div>
+      <div className="clothes-show-header__title"><p>Il piacere di indossare ciò che vi rende unici!</p></div>
+      <div className="clothes-show-header__intro"><p>Ciao utente. Ecco la selezone di capi in promozione esclusivi della tua taglia!</p></div>
     </div>
   );
 }
