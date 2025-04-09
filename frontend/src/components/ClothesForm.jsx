f17d86627a1ea82bb860b568740ed29e05d49d56
Ricardo <ricardobozzi23@gmail.com>
2025-04-09 18:46:52 +0200

    Added reset and spinner

--- frontend/src/components/ClothesForm.jsx
+++ frontend/src/components/ClothesForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, forwardRef, useImperativeHandle} from "react";
 import { useForm } from "react-hook-form";
 import { v4 as uuid } from "uuid";
 
