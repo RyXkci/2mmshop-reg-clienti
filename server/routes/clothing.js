ce2b50650599506e5597f2d65929c7d241da9ae0
Ricardo <ricardobozzi23@gmail.com>
2025-04-09 19:59:26 +0200

    Added delete endpoint and button

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const multer = require('multer');
 const {cloudinary, storage} = require('../cloudinary');
 const upload = multer({storage});
-const {getClothing, getSingleClothing, postClothing} = require('../controllers/clothingController');
+const {getClothing, getSingleClothing, postClothing, deleteClothing} = require('../controllers/clothingController');
 
 // get all clothing
 
@@ -18,5 +18,10 @@ router.get('/:id', getSingleClothing)
 //  post clothing
 
 router.post('/', upload.any(), postClothing);
+
+
+//delete clothing
+router.post('/delete', upload.any(), deleteClothing);
+
 
 module.exports = router;
